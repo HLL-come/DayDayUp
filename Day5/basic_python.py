1220bf389d3e63b7207c1ca219a93f09a749277c
Linglin <he.linglin@gmail.com>
2023-04-23 12:01:50 +0800

    before lunch

--- Day5/basic_python.py
+++ Day5/basic_python.py
@@ -1,19 +1,21 @@
-# # Print a simple string
-# print("Output #1: I'm excited to learn Python.")
+# Print a simple string
+print("Output #1: I'm excited to learn Python.")
 
-# # Add two lists together
-# a = [1, 2, 3, 4]
-# b = ["first", "second", "third", "fourth"]
-# c = a + b
+# Add two lists together
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+c = a + b
+
+print("{0}, {1}, {2}".format(a,b,c))
 
 # e = "Output #3: {0}, {1}, {2}".format(a, b, c)
 
 # print(e)
 
-# # print("Output #3: {0}, {1}, {2}".format(a, b, c))
+# print("Output #3: {0}, {1}, {2}".format(a, b, c))
 # print(a,b,c)
 
-xteam = [1,2,3]
+# xteam = [1,2,3]
 
-ßfor i in xteam:
-    print(i)
+# for i in xteam:
+#     print(i)
---
+++ Day5/importMathod.py
@@ -0,0 +1,7 @@
+from math import sqrt, log, exp
+
+print("{0:.4f}".format(log(3)))
+print("{0:.2f}".format(exp(5)))
+print("{0:.2f}".format(sqrt(5)))
+
+print("Output #14: {0:s}".format('I\'m enjoying learning Python'))
