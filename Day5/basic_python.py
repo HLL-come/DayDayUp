0cea265ace2aabaecca4bc8928cdd139be1a6976
Linglin <he.linglin@gmail.com>
2023-04-23 10:53:52 +0800

    good

---
+++ Day5/basic_python.py
@@ -0,0 +1,19 @@
+# # Print a simple string
+# print("Output #1: I'm excited to learn Python.")
+
+# # Add two lists together
+# a = [1, 2, 3, 4]
+# b = ["first", "second", "third", "fourth"]
+# c = a + b
+
+# e = "Output #3: {0}, {1}, {2}".format(a, b, c)
+
+# print(e)
+
+# # print("Output #3: {0}, {1}, {2}".format(a, b, c))
+# print(a,b,c)
+
+xteam = [1,2,3]
+
+ßfor i in xteam:
+    print(i)
